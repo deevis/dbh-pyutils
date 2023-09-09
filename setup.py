a42e731155e9ecc134e9582afd5389b42cc15059
Darren <darren.hicks@gmail.com>
2023-09-09 16:51:38 -0600

    Standardizing on dbh_utils for name

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 setup(
-    name='dbh-pyutils',
-    packages=find_packages(include=['dbh-pyutils']),
+    name='dbh_pyutils',
+    packages=find_packages(include=['dbh_pyutils']),
     version='0.1.0',
     description='DBH Utils Python library',
     author='Darren Hicks',
     license='MIT',
-    install_requires=['python-dotenv', 'chromadb', 'pytube'],
+    install_requires=['python-dotenv', 'chromadb', 'pytube', 'numpy==1.23'],
     # setup_requires=['pytest-runner'],
     # tests_require=['pytest==4.4.1'],
     # test_suite='tests',
