396881c1951c2af58ff4120023c4e54dda26992b
Darren <darren.hicks@gmail.com>
2023-09-10 19:07:08 -0600

    Enhance VidsquidMarkup to allow checking for available work prior to loading TextGenerationWebuiAPI

--- dbh_pyutils/dbh_vidsquid_markup.py
+++ dbh_pyutils/dbh_vidsquid_markup.py
@@ -5,21 +5,34 @@ from dbh_pyutils.dbh_text_generation_webui_api import TextGenerationWebuiAPI
 from dbh_pyutils.dbh_prompt_builder import PromptBuilder
 
 class VidsquidMarkup():
-    def __init__(self, llm_api, 
-                 vidsquid_url="http://192.168.0.43:3143",
+    def __init__(self, llm_model_name: str, 
+                 vidsquid_url: str ="http://192.168.0.43:3143",
+                 min_whisper_text_length: int = 200,
                  debug=False):
-        self.debug = debug
-        self.llm_api = llm_api
-        self.prompt_builder = PromptBuilder(llm_api)
+        self.llm_model_name = llm_model_name
         self.vidsquid_url = vidsquid_url
+        self.min_whisper_text_length = min_whisper_text_length
+        self.debug = debug
+        self.llm_api = None # defer until we know there is work to do
+        self.prompt_builder = None # defer until we know there is work to do
 
+    def get_llm_api(self):
+        if self.llm_api is None:
+            self.llm_api = TextGenerationWebuiAPI(self.llm_model_name, debug=self.debug)
+        return self.llm_api
+
+    def get_prompt_builder(self):
+        if self.prompt_builder is None:
+            self.prompt_builder = PromptBuilder(self.get_llm_api(), debug=self.debug)
+        return self.prompt_builder
+    
     def get_inference_config(self):
         config = {
-            'generating_model_name': self.llm_api.get_llm_name(),
-            'llm_params': self.llm_api.get_llm_params(),
+            'generating_model_name': self.get_llm_api().get_llm_name(),
+            'llm_params': self.get_llm_api().get_llm_params(),
         }
         for template in self.get_inference_template():
-            prompt = getattr(self.prompt_builder, template[1])("{TEXT}", template[2], promptOnly=True)
+            prompt = getattr(self.get_prompt_builder(), template[1])("{TEXT}", template[2], promptOnly=True)
             config[template[0]] = {
                 'prompt': prompt,
                 'sub_type': template[2]
@@ -37,12 +50,19 @@ class VidsquidMarkup():
             ['hashtags_1', 'infer_hashtags', None],
         ]
     
-    # Will find videos that have no AI markup and generate it
-    def do_ai_markup(self, min_whisper_text_length: int = 200):
-        generating_model_name = self.llm_api.get_llm_name()
-        print(f"Using model: {generating_model_name}")
+    def is_work_for_model(self):
         endpoint = f'videos/list_no_ai_markup_for_model_videos'
-        url = f"{self.vidsquid_url}/{endpoint}?generating_model_name={generating_model_name}&min_whisper_txt_length={min_whisper_text_length}"
+        url = f"{self.vidsquid_url}/{endpoint}?generating_model_name={self.llm_model_name}&min_whisper_txt_length={self.min_whisper_text_length}"
+        print(f"Checking for work at: {url}")
+        r = requests.get(url)
+        result = r.json()
+        return result['count'] > 0
+
+    # Will find videos that have no AI markup and generate it
+    def do_ai_markup(self):
+        print(f"Using model: {self.llm_model_name}")
+        endpoint = f'videos/list_no_ai_markup_for_model_videos'
+        url = f"{self.vidsquid_url}/{endpoint}?generating_model_name={self.llm_model_name}&min_whisper_txt_length={self.min_whisper_text_length}"
         print(f"Getting videos needing tagging from: {url}")
         r = requests.get(url)
         result = r.json()
@@ -68,12 +88,12 @@ class VidsquidMarkup():
             if len(whisper_txt) >= min_whisper_text_length:
                 timings = {}
                 payload = {
-                    'generating_model_name': generating_model_name,
+                    'generating_model_name': self.llm_model_name,
                     'timings': timings
                 }
 
                 for template in self.get_inference_template():
-                    response = getattr(self.prompt_builder, template[1])(whisper_txt, template[2])
+                    response = getattr(self.get_prompt_builder(), template[1])(whisper_txt, template[2])
                     payload[template[0]] = response
                     timings[template[0]] = self.llm_api.last_query_time
                     print(f"{template[0]}[{self.llm_api.last_query_time}]: {response}")
