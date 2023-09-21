9a29fc213fe279d47d7266fa4b4a24c15480863c
Darren <darren.hicks@gmail.com>
2023-09-21 08:37:20 -0600

    Version 0.1.3

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import find_packages, setup
 setup(
-    name='dbh_pyutils',
-    packages=find_packages(include=['dbh_pyutils']),
-    version='0.1.0',
+    name='dbh-pyutils',
+    packages=find_packages(),
+    version='0.1.3',
     description='DBH Utils Python library',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     author='Darren Hicks',
+    author_email="darren.hicks@gmail.com",
     license='MIT',
-    install_requires=['python-dotenv', 'chromadb', 'pytube', 'numpy==1.23', 'nltk', 'langchain'],
+    install_requires=['chromadb', 'pytube', 'nltk', 'langchain'],
+    python_requires='>=3.10',
+    url='https://github.com/deevis/dbh-pyutils'
     # setup_requires=['pytest-runner'],
     # tests_require=['pytest==4.4.1'],
     # test_suite='tests',
