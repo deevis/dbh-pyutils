24ede7705a3f995a7d2466b60a96d2a9ffe6a7f9
Darren <darren.hicks@gmail.com>
2024-02-18 01:11:39 -0700

    Version 0.2.0 since textgenerationwebui no longer supports legacy Kobold API (OpenAI API only now

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='dbh-pyutils',
     packages=find_packages(),
-    version='0.1.10',
+    version='0.2.0',
     description='DBH Utils Python library',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
